4347b1c0818985e2b8d15e6750123a842bb3e97d
Evgeni <666997@bk.ru>
2022-04-11 09:50:02 +0800

    Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = -3;
+
+if(a > 0)
+{
+    if(a % 2 > 0)
+    {
+        Console.WriteLine($"Число a = {a} - нечетное");
+    }
+    else
+    {
+        Console.WriteLine($"Число a = {a} - четное");
+    }
+}
+else
+{
+    if(a * (-1) % 2 > 0)
+    {
+        Console.WriteLine($"Число a = {a} - нечетное");
+    }
+    else
+    {
+        Console.WriteLine($"Число a = {a} - четное");
+    }
+}
