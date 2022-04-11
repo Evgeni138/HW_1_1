32a3e2fa57e082dc620f40209738333f1f230aec
Evgeni <666997@bk.ru>
2022-04-11 10:21:09 +0800

    Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 8, b =1;
+
+while(b +1 < a)
+{
+    if(b % 2 > 0)
+    {
+        b ++;
+    }
+    else
+    {
+        Console.Write($"{b}, ");
+        b++;
+    }
+}
+if( a % 2 < 1)
+{
+    Console.Write(a);
+}
+else
+{
+    Console.Write(a - 1);
+}
