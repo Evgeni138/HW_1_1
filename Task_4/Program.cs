aa1a3b59ad4b88d285b4b9d3ea77e9f1eeba274d
Evgeni <666997@bk.ru>
2022-04-11 09:33:36 +0800

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,28 @@
+﻿int a = 2, b = 3, c = 7;
+int max = a;
+
+if(a > b)
+{
+    if(a > c)
+        {
+            Console.WriteLine($"Максимальное число a = {max}");
+        }
+    else 
+    {
+        max = c;
+        Console.WriteLine($"Максимальное число c = {max}");
+    }
+}
+else
+{
+    if(b > c)
+    {
+        max = b;
+        Console.WriteLine($"Максимальное число b = {max}");
+    }
+    else 
+    {
+        max = c;
+        Console.WriteLine($"Максимальное число c = {max}");
+    }
+}
